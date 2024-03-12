0fd8da6daf6225d7133775f78c4108f09ed46c11
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-12 17:40:08 +0100

    build(compileSdk): Upgrade compile Sdk from 33 to 34.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.openclassrooms.tajmahal"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.openclassrooms.tajmahal"
