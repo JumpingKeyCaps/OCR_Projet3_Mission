3843a6d08f48440d4640e99a057be083fd1b3d80
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-14 18:56:27 +0100

    test(model.domain): add MainActivity unit tests.

---
+++ app/src/test/java/com/openclassrooms/tajmahal/uiUnitTest/MainActivityUnitTest.java
@@ -0,0 +1,21 @@
+package com.openclassrooms.tajmahal.uiUnitTest;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+
+/**
+ * Test of the Main Activity
+ * Unitary test is not the best way to test an activity
+ * due to the reliance with android framework an UI components.
+ *
+ * its better to use Espresso or Robolectric to done this job.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class MainActivityUnitTest {
+
+
+
+
+}
