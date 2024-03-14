209f8bc6963dd0843714371db564d1e644a67e95
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-14 18:58:16 +0100

    build(gradle): add robolectric dependencies. - add mockito dependencies. - add androidX core-testing dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,12 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
+    testOptions{
+        unitTests{
+            isIncludeAndroidResources  = true
+        }
+    }
 }
 
 dependencies {
@@ -57,6 +63,8 @@ dependencies {
     //tests
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:4.0.0")
+    testImplementation("org.robolectric:robolectric:4.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
 }
