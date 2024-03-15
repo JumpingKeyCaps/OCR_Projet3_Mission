484dad86c8b44b877e1344434aa7f498bbb6a5c0
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-15 15:22:31 +0100

    fix(ui/restaurant): reworking of the details ViewModel.

--- app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/DetailsViewModel.java
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/DetailsViewModel.java
@@ -3,15 +3,21 @@ package com.openclassrooms.tajmahal.ui.restaurant;
 import android.content.Context;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
 import com.openclassrooms.tajmahal.R;
 import com.openclassrooms.tajmahal.data.repository.RestaurantRepository;
+import com.openclassrooms.tajmahal.data.repository.ReviewsRepository;
 import com.openclassrooms.tajmahal.domain.model.Restaurant;
+import com.openclassrooms.tajmahal.domain.model.Review;
+import com.openclassrooms.tajmahal.domain.model.ReviewsSummary;
 
 import javax.inject.Inject;
 
 import java.util.Calendar;
+import java.util.List;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
 
@@ -25,15 +31,23 @@ import dagger.hilt.android.lifecycle.HiltViewModel;
 public class DetailsViewModel extends ViewModel {
 
     private final RestaurantRepository restaurantRepository;
+    private final ReviewsRepository reviewsRepository;
 
+    private final MutableLiveData<List<Review>> reviews = new MutableLiveData<>();
+
+    final LiveData<ReviewsSummary> reviewsSummary = Transformations.map(reviews, input ->
+            new ReviewsSummary(reviews.getValue())
+    );
     /**
      * Constructor that Hilt will use to create an instance of MainViewModel.
      *
      * @param restaurantRepository The repository which will provide restaurant data.
      */
     @Inject
-    public DetailsViewModel(RestaurantRepository restaurantRepository) {
+    public DetailsViewModel(RestaurantRepository restaurantRepository, ReviewsRepository reviewsRepository) {
         this.restaurantRepository = restaurantRepository;
+        this.reviewsRepository = reviewsRepository;
+
     }
 
     /**
@@ -82,5 +96,16 @@ public class DetailsViewModel extends ViewModel {
         }
         return dayString;
     }
+
+
+    /**
+     * Methode to fetch all reviews from reviews repository
+     */
+    public void fetchReviews(){
+        //Set the value of the reviews livedata
+        reviews.setValue(reviewsRepository.getReviews().getValue());
+
+    }
+
 
 }
