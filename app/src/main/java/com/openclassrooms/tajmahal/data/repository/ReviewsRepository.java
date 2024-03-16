55d77247b4ea290686a5888b393af754897fc77e
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-16 23:39:49 +0100

    docs(data/repository): Add JavaDoc and code cleaning.

--- app/src/main/java/com/openclassrooms/tajmahal/data/repository/ReviewsRepository.java
+++ app/src/main/java/com/openclassrooms/tajmahal/data/repository/ReviewsRepository.java
@@ -14,21 +14,22 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 /**
- * This is the repository class for managing restaurant reviews. Repositories are responsible
- * for coordinating data operations from data sources such as network APIs, databases, etc.
+ * This is the repository class for managing restaurant reviews.
+ * <p>
+ * Repositories are responsible for coordinating data operations from data sources such as network APIs, databases, etc.
  * Typically in an Android app built with architecture components, the repository will handle
  * the logic for deciding whether to fetch data from a network source or use data from a local cache.
- *
+ * <p>
  */
 @Singleton
 public class ReviewsRepository {
-    // The API interface instance that will be used for network requests related to restaurant data.
+    // The API interface instance that will be used for network requests related to reviews data.
     private final RestaurantApi restaurantApi;
 
     /**
      * Constructs a new instance of {@link ReviewsRepository} with the given {@link RestaurantApi}.
      *
-     * @param restaurantApi The network API interface for fetching restaurant data.
+     * @param restaurantApi The network API interface for fetching reviews data.
      */
     @Inject
     public ReviewsRepository(RestaurantApi restaurantApi) {
@@ -40,20 +41,19 @@ public class ReviewsRepository {
      * This method will make a network call using the provided {@link RestaurantApi} instance
      * to fetch all reviews of the restaurant.
      *
-     * @return LiveData holding the list of restaurant reviews.
+     * @return  a LiveData holding the list of restaurant reviews.
      */
     public LiveData<List<Review>> getReviews(){
         return new MutableLiveData<>(restaurantApi.getReviews());
     }
 
-
     /**
-     * add a new review to the restaurant.
+     * Add a new review to the restaurant reviews list.
      * <p>
      * This method will usually be connected to a network call or database query in its
-     * implementing class, add a review to the list of the existing reviews.
+     * implementing class, to add a new review to the list of the existing reviews.
      * </p>
-     * @param newReview the review to add in the reviews list
+     * @param newReview the {@link Review} to add in the reviews list
      */
     public void addReview(@NonNull Review newReview){
         restaurantApi.addReview(newReview);
