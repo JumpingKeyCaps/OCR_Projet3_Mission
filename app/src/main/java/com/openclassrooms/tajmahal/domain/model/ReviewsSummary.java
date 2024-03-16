9459027a33364fc58bc65ada83c19bea7aa14fd4
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-17 00:24:47 +0100

    docs(domain.model): Added the JavaDoc and cleaned up the code.

--- app/src/main/java/com/openclassrooms/tajmahal/domain/model/ReviewsSummary.java
+++ app/src/main/java/com/openclassrooms/tajmahal/domain/model/ReviewsSummary.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
- * Represents all stats about the restaurant reviews.
+ * Represents all restaurant review statistics.
  * This class encapsulates the stats of all users posted reviews, including the average rating of the reviewers,
  * the number of notes for each available note, and the total of reviews submitted.
  */
@@ -21,17 +21,20 @@ public class ReviewsSummary {
     /** The average rating of the restaurant. */
     private float averageScore;
 
-    /** The number of reviews submitted to the restaurant. */
+    /** The number of reviews submitted about the restaurant. */
     private int totalReviewsCount;
 
-    /** The Mapping :  number of rating by available ratings .
+    /** The Mapping :  number of reviews by available ratings .
      * for a Map<key,value> we have :
-     *  -Key: rating value.
-     *  -value: number of review for this rating.
+     *  - Key: Rating value.
+     *  - Value: Number of reviews for this rating.
      * */
     private Map<Integer, Integer> numberOfReviewsForEachScore;
 
 
+    /**
+     * Note maximum and minimum allowed.
+     */
     private static final int NOTE_MIN = 1;
     private static final int NOTE_MAX = 5;
 
@@ -43,7 +46,7 @@ public class ReviewsSummary {
      * @param reviewsList the reviews list of the restaurant.
      */
     public ReviewsSummary(List<Review> reviewsList) {
-
+        //Using in-class methods to do the data manipulation work directly inside the constructor.
         this.averageScore = calculateAverageRating(reviewsList);
         this.totalReviewsCount = reviewsList.size();
         this.numberOfReviewsForEachScore = getReviewCountByRating(reviewsList);
@@ -52,60 +55,60 @@ public class ReviewsSummary {
 
     // ---- work methods
 
-
     /**
-     * Calcule le nombre de notes par note pour une liste de reviews.
+     * Calculate the number of reviews per rating for a list of reviews.
      *
-     * @param reviews La liste des reviews.
-     * @return Une Map contenant le nombre de notes pour chaque note de 1 à 5.
+     * @param reviews the list of reviews to process.
+     * @return A Map containing the number of reviews for each note from 1 to 5.
      */
     public static Map<Integer, Integer> getReviewCountByRating(List<Review> reviews) {
-        Map<Integer, Integer> nombreNotesParNote = new HashMap<>();
-        //on map nos notes possibles de 1 a 5
+        Map<Integer, Integer> numberReviewByRating = new HashMap<>();
+        //Map our possible grades from 1 to 5
         for (int i = NOTE_MIN; i <= NOTE_MAX; i++) {
-            nombreNotesParNote.put(i, 0);
+            numberReviewByRating.put(i, 0);
         }
-        //pour chaque review, on incremente pour chaque note, le total de reviews correspondantes.
+        //For each review, we increment for each rating, the total of corresponding reviews.
         for (Review review : reviews) {
             int note = review.getRate();
             if (note >= NOTE_MIN && note <= NOTE_MAX) {
-                nombreNotesParNote.put(note, nombreNotesParNote.get(note) + 1);
+                numberReviewByRating.put(note, numberReviewByRating.get(note) + 1);
             }
         }
-        return nombreNotesParNote;
+        return numberReviewByRating;
     }
 
     /**
-     * Calculate the average rating.
+     * Calculate the average rating of the restaurant.
      *
-     * @param reviews the reviews list.
-     * @return a float with a average rate between 1 to 5.
+     * @param reviews the reviews list of the restaurant.
+     * @return the average rating of the restaurant, between 1 to 5.
      */
     public float calculateAverageRating(List<Review> reviews){
-        //on check que la liste n'est pas vide
+        //check that the list is not empty
         if(reviews.isEmpty()) return 0.0f;
-        //on calcul la note total de toutes les reviews
+        //calculate the total score of all reviews
         float totalScore = 0.0f;
         for (Review review : reviews) {
             totalScore += review.getRate();
         }
-        //on return la valeur de la moyenne arrondi a 1 chiffre apres la virgule
+        //returns the value of the average rounded to 1 decimal place after the decimal point.
         return Math.round((totalScore/reviews.size()) * 10) / 10f;
     }
 
     /**
-     * Calculate the average rating. V2
-     *
+     * Calculate the average rating. v2
+     *<p>
      * maybe better ?
      * (use stream() and average() with default value)
+     *</p>
      *
-     * @param reviews the reviews list.
-     * @return a float with a average rate between 1 to 5.
+     * @param reviews the reviews list of the restaurant.
+     * @return the average rating of the restaurant, between 1 to 5.
      */
     public float calculateAverageRatingV2(List<Review> reviews){
-        //on check que la liste n'est pas vide
+        //check that the list is not empty
         if(reviews.isEmpty()) return 0.0f;
-        //on return la valeur de la moyenne arrondie a 1 chiffre apres la virgule, avec une val par dflt.
+        //returns the value of the average rounded to 1 decimal place after the decimal point, with a secure default value.
         return Math.round((reviews.stream().mapToInt(Review::getRate).average().orElse(0.0f)) * 10) / 10f;
     }
 
@@ -113,48 +116,44 @@ public class ReviewsSummary {
     /**
      * Forge a String via a text binding of total number reviews value .
      *
-     * @param context the context to use getString().
-     * @return a formated String displaying the data.
+     * @param context a context to use getString().
+     * @param totalReviewsCount the number of reviews of the restaurant.
+     * @return a formatted String displaying the data.
      */
     public String totalNumberOfReviewsStringFormat(Context context, int totalReviewsCount){
         String reviewsCountInText;
         try{
-            //getString peut generer une erreur si le context retourner par  getContext() est null
+            //Use of preformatted text resource.
             reviewsCountInText = context.getString(R.string.restaurant_total_reviews_format_txt,String.valueOf(totalReviewsCount));
         }catch (NullPointerException e){
-            //on fait le binding manuelement en cas derreur
+            //do the binding manually in case of error
             reviewsCountInText = "("+totalReviewsCount+")";
         }
         return reviewsCountInText;
     }
 
     /**
-     *
-     * create a  safe(with default value) list of String,
-     * where index is a rate, and value the number of reviews with this rating.
-     *
+     * Create a safe (with default value) list of String,
+     * where the index is a rating, and the value is the number of reviews with this rating.
+     *<p>
      * /!\ Warning for use the returned list: /!\
      * index start at 0 --> 1* rating
      *                1 --> 2* rating
      *                etc...
+     *</p>
      *
-     * @return a list of int representing the number of reviews for each rate.
+     * @return a list of Integer representing the number of reviews for each rating.
      */
     public List<Integer> safeProgressRatingBarsValuesListGenerator(){
         List<Integer> progressOfRatingsBars = new ArrayList<>();
         for(int i=0;i<5;i++){
             int numberOfReviewsForThisRating;
-            try{
-                //on recupere la map de nos note par note et on creer une liste plus simple a manipuler avec des valeurs par default.
-                numberOfReviewsForThisRating = numberOfReviewsForEachScore.getOrDefault(i+1,0);
-            }catch (NullPointerException e){
-                numberOfReviewsForThisRating = 0;
-            }
+            //retrieve the map of the reviews by note and create a list with default values, that is easier to handle.
+            numberOfReviewsForThisRating = numberOfReviewsForEachScore.getOrDefault(i+1,0);
             progressOfRatingsBars.add(numberOfReviewsForThisRating);
         }
         return progressOfRatingsBars;
     }
-
 
 
 
@@ -180,16 +179,16 @@ public class ReviewsSummary {
 
 
     /**
-     * Return the total count of reviews.
+     * Return the total number of reviews for the restaurant.
      *
-     * @return a int representing the number of reviews
+     * @return a int representing the number of reviews.
      */
     public int getTotalReviewsCount() {
         return totalReviewsCount;
     }
 
     /**
-     * Set or update the total count of reviews for the restaurant.
+     * Set or update the total number of reviews for the restaurant.
      *
      * @param totalReviewsCount the total of reviews submitted
      */
@@ -215,7 +214,6 @@ public class ReviewsSummary {
     public void setNumberOfReviewsForEachScore(Map<Integer, Integer> numberOfReviewsForEachScore) {
         this.numberOfReviewsForEachScore = numberOfReviewsForEachScore;
     }
-
 
 
 
