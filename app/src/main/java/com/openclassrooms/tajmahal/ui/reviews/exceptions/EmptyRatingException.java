68af3439b63190ca13278ae76af2b07982f4433a
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-19 16:50:45 +0100

    doc(ui/reviews/exceptions): Added the JavaDoc.

--- app/src/main/java/com/openclassrooms/tajmahal/ui/reviews/exceptions/EmptyRatingException.java
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/reviews/exceptions/EmptyRatingException.java
@@ -1,5 +1,8 @@
 package com.openclassrooms.tajmahal.ui.reviews.exceptions;
 
+/**
+ *  Custom Exception class to manage a review without a rating.
+ */
 public class EmptyRatingException extends Exception {
     public EmptyRatingException(){
         super();
