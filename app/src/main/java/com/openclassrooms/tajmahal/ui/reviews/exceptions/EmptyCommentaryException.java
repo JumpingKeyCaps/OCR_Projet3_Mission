c5fe8f2e0302ebb553ccd4eefc89384ea584246f
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-19 16:50:59 +0100

    doc(ui/reviews/exceptions): Added the JavaDoc.

--- app/src/main/java/com/openclassrooms/tajmahal/ui/reviews/exceptions/EmptyCommentaryException.java
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/reviews/exceptions/EmptyCommentaryException.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.tajmahal.ui.reviews.exceptions;
-
+/**
+ *  Custom Exception class to manage a empty commentary text.
+ */
 public class EmptyCommentaryException extends Exception {
     public EmptyCommentaryException(){
         super();
