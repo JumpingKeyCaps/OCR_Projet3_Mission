5801f8d7f75784b6a3835e9e5709877269e45942
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-19 16:50:31 +0100

    doc(ui/reviews/exceptions): Added the JavaDoc.

--- app/src/main/java/com/openclassrooms/tajmahal/ui/reviews/exceptions/TooLongCommentaryException.java
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/reviews/exceptions/TooLongCommentaryException.java
@@ -1,5 +1,8 @@
 package com.openclassrooms.tajmahal.ui.reviews.exceptions;
 
+/**
+ *  Custom Exception class to manage a too long commentary text.
+ */
 public class TooLongCommentaryException extends Exception {
 
     public TooLongCommentaryException(){
