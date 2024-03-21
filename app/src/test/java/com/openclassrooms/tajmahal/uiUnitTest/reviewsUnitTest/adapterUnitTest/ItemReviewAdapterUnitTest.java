6b576825cb734cc1a2ccde97a1f0d24d6b43f56c
Samuel Lebailly <sam.meusa@gmail.com>
2024-03-21 17:06:36 +0100

    test(ui/reviews/adapter): fix some unit tests.

--- app/src/test/java/com/openclassrooms/tajmahal/uiUnitTest/reviewsUnitTest/adapterUnitTest/ItemReviewAdapterUnitTest.java
+++ app/src/test/java/com/openclassrooms/tajmahal/uiUnitTest/reviewsUnitTest/adapterUnitTest/ItemReviewAdapterUnitTest.java
@@ -3,18 +3,14 @@ package com.openclassrooms.tajmahal.uiUnitTest.reviewsUnitTest.adapterUnitTest;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.TestCase.assertEquals;
 
-import static org.mockito.Mockito.when;
 
 
-import com.openclassrooms.tajmahal.domain.model.Review;
 import com.openclassrooms.tajmahal.ui.reviews.adapter.ItemReviewAdapter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.List;
 
 /**
  * Test of the Adapter for the RecyclerView of reviews fragment.
@@ -22,25 +18,15 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class ItemReviewAdapterUnitTest {
 
-    @Mock
-    private List<Review> reviewsList;
 
     /**
      * Test Constructor of the adapter
      */
     @Test
-    public void constructor_setsReviewsList() {
-        // Arrange : create a list of reviews
-        reviewsList.add(new Review("","","",1));
-        reviewsList.add(new Review("","","",1));
-        reviewsList.add(new Review("","","",1));
-        reviewsList.add(new Review("","","",1));
-
-        //Act: create a new adapter instance with the list
-        ItemReviewAdapter adapter = new ItemReviewAdapter(reviewsList);
-
+    public void constructor_ReviewsList() {
+        //Act: create a new adapter instance
+        ItemReviewAdapter adapter = new ItemReviewAdapter();
         // Assert: test if the adapter is well construct
-        assertEquals(reviewsList.size(), adapter.getItemCount());
         assertNotNull(adapter);
     }
 
@@ -58,15 +44,11 @@ public class ItemReviewAdapterUnitTest {
     @Test
     public void getItemCount_returnsReviewsCount() {
         // Arrange
-        int expectedCount = 3;
-        when(reviewsList.size()).thenReturn(expectedCount);
-        ItemReviewAdapter adapter = new ItemReviewAdapter(reviewsList);
-
+        ItemReviewAdapter adapter = new ItemReviewAdapter();
         // Act
         int actualCount = adapter.getItemCount();
-
         // Assert
-        assertEquals(expectedCount, actualCount);
+        assertEquals(0, actualCount);
     }
 
 }
